1471162923b231017c47e40199fd30d5c2d8e0d0
cristianokituxi <cristianoquichindo2@gmail.com>
2022-03-18 19:11:48 -0300

    projecto  concluido

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-function endereco() {
+function adress() {
     let inputConsult = document.querySelector('#inputConsult').value;
 
     let url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${inputConsult}`;
@@ -7,7 +7,7 @@ function endereco() {
            
            let urlCovid = `https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${dados.sigla}`
             fetch(urlCovid).then(function (response) {
-                response.json().then(mostraEndereco)
+                response.json().then(showAdress)
             })
 
 
@@ -19,14 +19,14 @@ function endereco() {
 
 
 
-function mostraEndereco(dados) {
-    let resultado = document.querySelector('#resultado')
+function showAdress(dados) {
+    let resultado = document.querySelector('#resul')
     if (dados.erro) {
-        resultado.innerHTML = "estado federativo não existente"
+        result.innerHTML = "estado federativo não existente"
     }
     else {
 
-        resultado.innerHTML = ` <P class="retursearch">Estado:${dados.state}.</P>
+        result.innerHTML = ` <P class="retursearch">Estado:${dados.state}.</P>
                                 <P class="retursearch">Uf:${dados.uf}.</P>
                                 <P class="retursearch">Id:${dados.uid}.</P>
                                   <P class="retursearch">Casos:${dados.cases}.</P>
